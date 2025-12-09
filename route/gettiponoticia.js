9e6f5c00cb1f7a6b0759702187c198f925bc7ad6
luxca-vltas <149402591+LuscaIF@users.noreply.github.com>
2025-12-09 15:15:57 -0400

    Add files via upload

--- route/gettiponoticia.js
+++ route/gettiponoticia.js
@@ -1,10 +1,12 @@
 module.exports = (app) => {
-    app.get('/noticias/tiponoticia/:tipo', async (req, res) => {
+    app.get('/noticias/tipo/:tiponoticia', async (req, res) => {
         try {
-            const tipo = req.params.tipo
+            const tiponoticia = req.params.tiponoticia
             await app.DBClient.connect(); //realizar a conexão com o banco 
             const noticias = await app.DBClient.db('portalnoticias')
-                .collection('noticias').find({ tiponoticia: new RegExp(tipo,'i')}).toArray();
+            .collection('noticias').find(
+                {tiponoticias:new RegExp(tiponoticia,'i')}
+            ).toArray();
             res.json(noticias)
         } finally {
             // Ensures that the client will close when you finish/error
